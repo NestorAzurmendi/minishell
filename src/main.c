ec8fa67b2ea2a3c88423effbc0b5f98b3aa591da
NestorAzurmendi <nestortisen@gmail.com>
2020-09-21 17:43:08 +0200

    patiputa

--- src/main.c
+++ src/main.c
@@ -3,30 +3,55 @@
 /*                                                        :::      ::::::::   */
 /*   main.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: hellnhell <hellnhell@student.42.fr>        +#+  +:+       +#+        */
+/*   By: nazurmen <nazurmen@student.42madrid.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/09/20 18:29:03 by hellnhell         #+#    #+#             */
-/*   Updated: 2020/09/20 19:06:53 by hellnhell        ###   ########.fr       */
+/*   Updated: 2020/09/20 20:27:55 by nazurmen         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
 
-char    *read_line(t_tab *t)
+char	*read_line(t_tab *t)
 {
-    get_next_line(0, &t->line);
-    return(t->line);
-    
+	get_next_line(0, &t->line);
+	//if(t->line)
+	//	printf("%ls\n", (unsigned int *)t->line);
+	return(t->line);
+
 }
 
-int     main(char **env)
+char *read_line2(void)
 {
-    t_tab   *t;
-    
-    while (1)
-    {
-        ft_putstr_fd("marishell%", 1);
-        read_line(t);
-    }
+	char *buff;
+
+	get_next_line(0, &buff);
+	return (buff);
+
+}
+
+void	initt(t_tab *t)
+{
+	t->line = NULL;
+	t->path = NULL;
+}
+
+int		main(int argc, char **env)
+{
+	t_tab	*t;
+	char *line;
+
+	t = malloc (sizeof(t_tab));
+	initt(t);
+	(void)argc;
+	(void)env;
+	while (1)
+	{
+		ft_putstr_fd("marishell%", 1);
+	//	line = read_line2();
+		line = read_line(t);
+		printf("%s\n", line);
+
+	}
 }
--- src/minishell.h
+++ src/minishell.h
@@ -3,28 +3,30 @@
 /*                                                        :::      ::::::::   */
 /*   minishell.h                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: hellnhell <hellnhell@student.42.fr>        +#+  +:+       +#+        */
+/*   By: nazurmen <nazurmen@student.42madrid.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/09/20 18:44:16 by hellnhell         #+#    #+#             */
-/*   Updated: 2020/09/20 19:00:29 by hellnhell        ###   ########.fr       */
+/*   Updated: 2020/09/20 20:28:23 by nazurmen         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef MINISHELL_H
 # define MINISHELL_H
 
-# include   <stdio.h>
-# include   <unistd.h>
-# include   <string.h>
-# include   <stdlib.h>
+# include	"../libft/libft.h"
+# include	"../libft/get_next_line/get_next_line.h"
+# include	<stdio.h>
+# include	<unistd.h>
+# include	<string.h>
+# include	<stdlib.h>
 
-typedef struct      s_tab
+typedef struct		s_tab
 {
-    char    **line;
-    char    *path;
-}                   t_tab;
+	char	*line;
+	char	*path;
+}					t_tab;
 
-char    *read_line(t_tab *t);
+char	*read_line(t_tab *t);
 
 
 #endif
